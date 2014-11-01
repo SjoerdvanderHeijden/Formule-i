fcbdd481be3076271a8bad7b0966fdb2959f1f00
painter105 <painter105@users.noreply.github.com>
2014-11-02 00:11:03 +0100

    ++ Parking class

--- Rush Hour.py
+++ Rush Hour.py
@@ -2,32 +2,32 @@
 
 class Car(object):
     """
-    (vracht)auto
-    input: poslist = lijst met coordinaten van voertuig (variabele lengte),
-                     elke coordinaat opgeslagen als tuple.
+    Car / lorry object.
+    
+    input: posList = list with tuples with coordinates.
     """
-    def __init__(self, poslist):
-        self.poslist = poslist
+    def __init__(self, posList):
+        self.posList = posList
 
         # uirekenen van richting
-        if poslist[0][0] == poslist[1][0]:
+        if posList[0][0] == posList[1][0]:
             self.richting = 'h'
-        elif poslist[0][1] == poslist[1][1]:
+        elif posList[0][1] == posList[1][1]:
             self.richting = 'v'
         else:
-            raise ValueError("Ongeldige coordinaten")
+            raise ValueError("Invalid coordinates!")
 
     def getPos(self):
-        return self.poslist
+        return self.posList
 
     def getDirection(self):
         return self.richting
 
     def getLength(self):
-        return len(self.poslist)
+        return len(self.posList)
 
     def moveCar(self, distance):
-        crd = self.poslist
+        crd = self.posList
         
         if self.richting == 'h':
             for i in xrange(len(crd)):
@@ -38,20 +38,53 @@ class Car(object):
                 # x-coordinaten worden aangepast
                 crd[i] = (crd[i][0], crd[i][1] + distance)
                 
-        self.poslist = crd
+        self.posList = crd
 
     def isAt(self, pos):
         # @pos: type: tuple
-        return pos in self.poslist
+        return pos in self.posList
 
 class RedCar(Car):
     #het lijkt me het handigst als simulatie checkt of RedCar bij de uitgang is.
     pass
 
 class Parking(object):
-    def __init__(self, width, height, carlist):
-        # werk rustig verder hieraan als je wilt, ik ben gestopt (voorlopig :p)
-        pass
+    """
+    Parking space with cars on it.
+
+    input: width, height: integers. exitpos: tuple. carlist: list
+    """
+    def __init__(self, width, height, exitpos, carList):
+        self.width = width
+        self.height = height
+        self.exitpos = exitpos
+
+        # Parking representatie (lijst met lijsten).
+        # (x,y) aanroepen door: parkList[x][y]
+        self.parkList = [ [None for y in xrange(height)] for x in xrange(width)]
+
+        # Auto's worden geplaatst.
+        for car in carList:
+            posList = car.getPos()
+            
+            for pos in posList:
+                x, y = pos[0], pos[1]
+
+                try:
+                    if self.parkList[x][y] == None:
+                        self.parkList[x][y] = car
+                    else:
+                        raise ValueError("Double car placing!")
+                    
+                except IndexError:
+                    raise ValueError("Car out of parking range!")
+                
+
+    def getParking(self):
+        print self.parkList
+
+    def occupiedBy(self, pos):
+        return self.parkList[pos[0]][pos[1]]
 
 def runSimulation():
     pass
