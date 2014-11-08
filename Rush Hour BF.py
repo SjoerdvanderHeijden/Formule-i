f6808c39dc388c377180124f110bb203904558c2
painter105 <painter105@users.noreply.github.com>
2014-11-08 13:46:04 +0100

    Changes: +import copy
    moveCarInParking:
    	- When calling moveCarInParking it's not longer needed to give
    	  'car' argument (just upperLeftCoord + distance).
    
    	-> Returns newParking (in place of newParking.parkList)
    
    Parking string representation changed

--- Rush Hour BF.py
+++ Rush Hour BF.py
@@ -2,7 +2,7 @@
 # Contributors: Patrick Schilder, Sjoerd van der Heijden and Alix Dodu
 
 
-import math
+import math, copy
 
 
 class Car(object):
@@ -62,17 +62,21 @@ class RedCar(Car):
     """
     pass
 
+
+### ======================================================================= ###
+### ======================================================================= ###
+
+
 class Parking(object):
     """
     Parking space with cars on it.
 
-    input: width, height: integers. exitpos: tuple. carlist: list
+    input: width, height: integers. exitpos: tuple.
     """
     def __init__(self, width, height, exitPos):
         self.width = width
         self.height = height
         self.exitPos = exitPos
-        self.carList = []
 
         # Parking representation is a list of lists. The lists correspond to x-coordinates, and their indexes to y-coordinates.
         # Thus: Element at position (x,y) is found at parkList[x][y]
@@ -112,12 +116,11 @@ class Parking(object):
     def occupiedBy(self, pos):
         return self.parkList[pos[0]][pos[1]]
 
-    def moveCarInParking(self, car, upperLeftCoord, distance):
+    def moveCarInParking(self, upperLeftCoord, distance):
         """
         Moves a car, by making a copy of the parking instance and 
         changing the position of the car in parkList of the copy.
         
-        @car: car to be moved (instance of the car class)
         @upperLeftCoord: upper left coordinate of the car to be moved.
         (tuple containing two integers)
         @distance: distance the car is moved. Positive if the car is moved 
@@ -129,10 +132,12 @@ class Parking(object):
         (list of lists)
         """
         # Retrieves a list of tuples corresponding to the coordinates of the car
-        startPos = car.getPos()
+        car = self.occupiedBy(upperLeftCoord)
+        startPos = car.getPos(upperLeftCoord)
         # Makes a copy of the current parking. The new position of the car will 
         # be stored in this copy 
-        newParking = deepcopy(self)
+        newParking = copy.deepcopy(self)
+        
         
         if car.isHorizontal():
             # Moves the car to the RIGHT:
@@ -141,7 +146,7 @@ class Parking(object):
                 for x in range(startPos[-1][0] + 1, \
     startPos[-1][0] + 1 + distance):
                     #..checks wether is is still in the parking.
-                    if x >= width:
+                    if x >= self.width:
                         raise ValueError\
     ("Cannot move car trough the parking walls.")
                     #..checks wether the way is free of cars.
@@ -173,7 +178,7 @@ class Parking(object):
             if distance > 0:
                 for y in range(startPos[-1][1] + 1, \
     startPos[-1][1] + 1 + distance):
-                    if y >= length:
+                    if y >= self.length:
                         raise ValueError\
     ("Cannot move car trough the parking walls.")
                     if self.parkList[startPos[0][0]][y] != None:
@@ -199,18 +204,11 @@ class Parking(object):
             newParking.parkList[startPos[0][0]][startPos[-1][1]] = None
             newParking.parkList[startPos[0][0]][startPos[0][0]+distance] = car
             
-        return newParking.parkList
+        return newParking
 
 
     def __str__(self):
         output = self.parkList[:]
-
-        for x in xrange(self.width):
-            for y in xrange(self.height):
-                if output[x][y] == None:
-                    output[x][y] = '.'
-                else:
-                    output[x][y] = output[x][y].getName()
 
         print '*',
         for i in xrange(self.width):
@@ -220,26 +218,39 @@ class Parking(object):
         for y in xrange(self.height):
             print '*',
             for x in xrange(self.width):
-                print str(output[x][y]),
+                if type(output[x][y]) == Car:
+                    print str(output[x][y].getName()),
+                elif (x,y) == self.exitPos:
+                    print '@',
+                elif output[x][y] == None:
+                    print '.',
+                else:
+                    print 'R',
+
             print '*'
 
-        print '*',
-        for i in xrange(self.width):
-            print '*',
-        print '*'
+        endRow = ''
+        for i in xrange(self.width + 2):
+            endRow += '* '
 
-        return "Exit: "+ str(self.exitPos)
+        return endRow
                 
 
 def testMoveCarInParking():             
     audi = RedCar(2,True)
     seat = Car(2,False)
-    exitPos1 = (6,2)
+    exitPos1 = (5,2)
     parking1 = Parking(6,6,exitPos1)
-    parking1.addCar(audi, (0,0))
+    parking1.addCar(audi, (0,2))
     parking1.addCar(seat, (3,2))
-
     print parking1
+    
+    parking2 = parking1.moveCarInParking((0,2), 1)
+    print parking2
+
+    # a.k.a. botsing:
+    parking3 = parking2.moveCarInParking((1,2), 1)
+    print parking3
 
 testMoveCarInParking()
             
