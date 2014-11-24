6410708cc643a8969ebe0e6f0f7ee284b809f5c6
SjoerdvanderHeijden <s.vdh@live.nl>
2014-11-24 16:37:52 +0100

    Update Rush_Hour_BF.py

--- Rush_Hour_BF.py
+++ Rush_Hour_BF.py
@@ -397,7 +397,54 @@ def board_1():
         print board
 
     print 'Opgelost in:', len(boards)-1, ' stappen.'
-        
+
+
+def board_2():
+    h = True
+    v = False
+    
+    exitPos1 = (5,2)
+    parking1 = Parking(6,6,exitPos1)
+    
+    rood = RedCar(2,h)
+    parking1.addCar(rood,(2,2))
+    
+    blauw = Car(2, h)
+    parking1.addCar(blauw, (2,0))
+    
+    oranje = Car(2, h)
+    parking1.addCar(oranje, (4,0))
+    
+    oranje2 = Car(2,h)
+    parking1.addCar(oranje2, (1,1))
+    
+    groen = Car(2, h)
+    parking1.addCar(groen, (3,1))
+    
+    groen2 = Car(2, h)
+    parking1.addCar(groen2, (0, 3))
+    
+    blauw2 = Car(2, h)
+    parking1.addCar(blauw2, (2,3))
+    
+    groen3 = Car(2, h)
+    parking1.addCar(groen3, (4,4))
+    
+    oranje3 = Car(2,h)
+    parking1.addCar(oranje3, (4,5))
+    
+    oranje4 = Car(2,v)
+    parking1.addCar(oranje4, (0,4))
+    
+    cyan = Car(2,v)
+    parking1.addCar(cyan, (4,2))
+    
+    cyan2 = Car(2,v)
+    parking1.addCar(cyan2, (3,4))
+    
+    vracht = Car(3,v)
+    parking1.addCar(vracht, (5,1))
+
         
 def board_3():
     exitPos2 = (5,2)
@@ -445,6 +492,81 @@ def board_3():
     for board in BreadthFirstSimulation(parking1):
         print board
 
+
+def board_4():
+    h = True
+    v = False
+    
+    exitPos1 = (8,4)
+    parking1 = Parking(9,9,exitPos1)
+    
+    rood = RedCar(2,h)
+    parking1.addCar(rood,(1,4))
+    
+    groen = Car(2,v)
+    parking1.addCar(groen,(0,0))
+    
+    vracht = Car(3,h)
+    parking1.addCar(vracht,(1,0))
+    
+    vracht2 = Car(3,v)
+    parking1.addCar(vracht2, (5,0))
+    
+    vracht3 = Car(3,v)
+    parking1.addCar(vracht3, (3,1))
+    
+    vracht4 = Car(3,h)
+    parking1.addCar(vracht4, (6,1))
+    
+    vracht5= Car(3,v)
+    parking1.addCar(vracht5,(8,2))
+    
+    blauw = Car(2,h)
+    parking1.addCar(blauw, (0,3))
+    
+    vracht6 = Car(3,h)
+    parking1.addCar(vracht6, (5,3))
+    
+    cyan = Car(2,v)
+    parking1.addCar(cyan,(0,4))
+    
+    groen2 = Car(2,v)
+    parking1.addCar(groen2,(3,4))
+    
+    vracht7 = Car(3,v)
+    parking1.addCar(vracht7, (2,5))
+    
+    vracht8 = Car(3,h)
+    parking1.addCar(vracht8, (5,6))
+    
+    vracht9 = Car(3,v)
+    parking1.addCar(vracht9, (8,6))
+    
+    oranje = Car(2,h)
+    parking1.addCar(oranje, (0,6))
+    
+    blauw2 = Car(2,v)
+    parking1.addCar(blauw2, (3,6))
+    
+    groen3 = Car(2,h)
+    parking1.addCar(groen3, (4,6))
+    
+    blauw3 = Car(2,v)
+    parking1.addCar(blauw3, (0,7))
+    
+    oranje2 = Car(2,v)
+    parking1.addCar(oranje2,(4,7))
+    
+    vracht10 = Car(2,h)
+    parking1.addCar(vracht10, (1,8))
+    
+    cyan2 = Car(2,h)
+    parking1.addCar(cyan2, (5,8))
+    
+    groen4 = Car(2,h)
+    parking1.addCar(groen4, (7,8))
+
+
 def testMoveCarInParking():             
     audi = RedCar(2,True)
     seat = Car(2,False)
@@ -462,4 +584,171 @@ if __name__ == '__main__':
     #testMoveCarInParking()
     board_3()
 
-
+#if __name__ == '__main__':
+#    # Game #2
+#    h = True
+#    v = False
+#    
+#    exitPos1 = (5,2)
+#    parking1 = Parking(6,6,exitPos1)
+#    
+#    rood = RedCar(2,h)
+#    parking1.addCar(rood,(2,2))
+#    
+#    blauw = Car(2, h)
+#    parking1.addCar(blauw, (2,0))
+#    
+#    oranje = Car(2, h)
+#    parking1.addCar(oranje, (4,0))
+#    
+#    oranje2 = Car(2,h)
+#    parking1.addCar(oranje2, (1,1))
+#    
+#    groen = Car(2, h)
+#    parking1.addCar(groen, (3,1))
+#    
+#    groen2 = Car(2, h)
+#    parking1.addCar(groen2, (0, 3))
+#    
+#    blauw2 = Car(2, h)
+#    parking1.addCar(blauw2, (2,3))
+#    
+#    groen3 = Car(2, h)
+#    parking1.addCar(groen3, (4,4))
+#    
+#    oranje3 = Car(2,h)
+#    parking1.addCar(oranje3, (4,5))
+#    
+#    oranje4 = Car(2,v)
+#    parking1.addCar(oranje4, (0,4))
+#    
+#    cyan = Car(2,v)
+#    parking1.addCar(cyan, (4,2))
+#    
+#    cyan2 = Car(2,v)
+#    parking1.addCar(cyan2, (3,4))
+#    
+#    vracht = Car(3,v)
+#    parking1.addCar(vracht, (5,1))
+#    
+#
+#if __name__ == '__main__':
+#    # Game #3
+#    h = True
+#    v = False
+#    
+#    exitPos1 = (5,2)
+#    parking1 = Parking(6,6,exitPos1)
+#    
+#    rood = RedCar(2,h)
+#    parking1.addCar(rood,(0,2))
+#    
+#    blauw = Car(2, h)
+#    parking1.addCar(blauw, (1,0))
+#    
+#    oranje = Car(2,h)
+#    parking1.addCar(oranje,(1,1))
+#    
+#    groen = Car(2,h)
+#    parking1.addCar(groen, (4,1))
+#    
+#    groen2 = Car(2,h)
+#    parking1.addCar(groen2,(0,3))
+#    
+#    blauw2 = Car(2,h)
+#    parking1.addCar(blauw2, (3,3))
+#    
+#    groen3 = Car(2,h)
+#    parking1.addCar(groen3, (4,4))
+#    
+#    blauw3 = Car(2,v)
+#    parking1.addCar(blauw3, (3,1))
+#    
+#    cyan = Car(2,v)
+#    parking1.addCar(cyan, (2,2))
+#    
+#    cyan2 = Car(2,v)
+#    parking1.addCar(cyan2,(5,2))
+#    
+#    oranje2 = Car(2,v)
+#    parking1.addCar(oranje2, (0,4))
+#    
+#    groen3 = Car(2,v)
+#    parking1.addCar(groen3,(2,4))
+#    
+#    vracht = Car(3,h)
+#    parking1.addCar(vracht,(3,0))
+#
+#if __name__ == '__main__':
+#    # Game #4
+#    h = True
+#    v = False
+#    
+#    exitPos1 = (8,4)
+#    parking1 = Parking(9,9,exitPos1)
+#    
+#    rood = RedCar(2,h)
+#    parking1.addCar(rood,(1,4))
+#    
+#    groen = Car(2,v)
+#    parking1.addCar(groen,(0,0))
+#    
+#    vracht = Car(3,h)
+#    parking1.addCar(vracht,(1,0))
+#    
+#    vracht2 = Car(3,v)
+#    parking1.addCar(vracht2, (5,0))
+#    
+#    vracht3 = Car(3,v)
+#    parking1.addCar(vracht3, (3,1))
+#    
+#    vracht4 = Car(3,h)
+#    parking1.addCar(vracht4, (6,1))
+#    
+#    vracht5= Car(3,v)
+#    parking1.addCar(vracht5,(8,2))
+#    
+#    blauw = Car(2,h)
+#    parking1.addCar(blauw, (0,3))
+#    
+#    vracht6 = Car(3,h)
+#    parking1.addCar(vracht6, (5,3))
+#    
+#    cyan = Car(2,v)
+#    parking1.addCar(cyan,(0,4))
+#    
+#    groen2 = Car(2,v)
+#    parking1.addCar(groen2,(3,4))
+#    
+#    vracht7 = Car(3,v)
+#    parking1.addCar(vracht7, (2,5))
+#    
+#    vracht8 = Car(3,h)
+#    parking1.addCar(vracht8, (5,6))
+#    
+#    vracht9 = Car(3,v)
+#    parking1.addCar(vracht9, (8,6))
+#    
+#    oranje = Car(2,h)
+#    parking1.addCar(oranje, (0,6))
+#    
+#    blauw2 = Car(2,v)
+#    parking1.addCar(blauw2, (3,6))
+#    
+#    groen3 = Car(2,h)
+#    parking1.addCar(groen3, (4,6))
+#    
+#    blauw3 = Car(2,v)
+#    parking1.addCar(blauw3, (0,7))
+#    
+#    oranje2 = Car(2,v)
+#    parking1.addCar(oranje2,(4,7))
+#    
+#    vracht10 = Car(2,h)
+#    parking1.addCar(vracht10, (1,8))
+#    
+#    cyan2 = Car(2,h)
+#    parking1.addCar(cyan2, (5,8))
+#    
+#    groen4 = Car(2,h)
+#    parking1.addCar(groen4, (7,8))
